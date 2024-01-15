bb67be00a26e599bd95e8d720caf2ec784afc122
FranciscoZhou5 <fg45874587@proton.me>
2024-01-14 22:30:08 -0300

    feat: add mayu

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,5 +1,69 @@
 import { Client, Events } from "discord.js";
 
+const feminineAdjectives = [
+  "Amável",
+  "Querida",
+  "Meiga",
+  "Fofa",
+  "Doce",
+  "Encantadora",
+  "Adorável",
+  "Apaixonante",
+  "Cuidadosa",
+  "Atenciosa",
+  "Simpática",
+  "Afetuosa",
+  "Carinhosa",
+  "Fofinha",
+  "Queridinha",
+  "Graciosa",
+  "Amorosa",
+  "Charmosa",
+  "Cheirosa",
+  "Delicada",
+  "Encantadora",
+  "Generosa",
+  "Sorridente",
+  "Alegre",
+  "Radiante",
+  "Brilhante",
+  "Paciente",
+  "Sábia",
+  "Harmoniosa",
+  "Iluminada",
+  "Especial",
+  "Divina",
+  "Serenata",
+  "Tranquila",
+  "Divertida",
+  "Paz",
+  "Branda",
+  "Celestial",
+  "Pura",
+  "Gentil",
+  "Sonhadora",
+  "Deslumbrante",
+  "Doce",
+  "Pacífica",
+  "Polida",
+  "Perfeita",
+  "Serena",
+];
+
+const masculineAdjectives = [];
+
 export default (client: Client<boolean>) => {
-  client.once(Events.MessageCreate, () => {});
+  client.on(Events.MessageCreate, (interaction) => {
+    if (interaction.content.toLowerCase() === "a mayu é?") {
+      const randomIndex = Math.floor(Math.random() * feminineAdjectives.length);
+
+      interaction.reply(`${feminineAdjectives[randomIndex]}(às vezes).`);
+    }
+
+    // if (["o zé é?", "o ze é?", "o ze e?"].includes(interaction.content.toLowerCase())) {
+    //   const randomIndex = Math.floor(Math.random() * feminineAdjectives.length);
+
+    //   interaction.reply(`${feminineAdjectives[randomIndex]}(às vezes).`);
+    // }
+  });
 };
